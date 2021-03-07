6466d20ad39d213bab342b0f656e63ecc2b87d58
Jesse Stolwijk <stolwijkjesse@gmail.com>
2021-03-07 23:20:52 +0100

    Add some styling to the dropdown

--- ui/src/components/DropDown.tsx
+++ ui/src/components/DropDown.tsx
@@ -15,9 +15,15 @@ export interface Option {
 
 const DropDown: React.FC<DropDownProps> = ({ options, name, id, onValueSelected, value }) => {
   return (
-    <select name={name} id={id} value={value || options[0].value} onChange={(e) => onValueSelected(e.target.value)}>
+    <select
+      name={name}
+      id={id}
+      value={value || options[0].value}
+      onChange={(e) => onValueSelected(e.target.value)}
+      className="w-full border bg-white rounded px-3 py-2 outline-none"
+    >
       {options.map((option, index) => (
-        <option key={index} value={option.value}>
+        <option key={index} value={option.value} className="py-1">
           {option.title}
         </option>
       ))}
